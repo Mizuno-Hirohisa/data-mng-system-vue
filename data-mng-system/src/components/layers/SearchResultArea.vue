ec44ab23d8520fe4d1c540e91ca4729bd40efc89
tobabeti <hirohisa_mizuno721@outlook.jp>
2024-10-14 11:12:55 +0900

    SearchResultAreaの追加

---
+++ data-mng-system/src/components/layers/SearchResultArea.vue
@@ -0,0 +1,11 @@
+<script setup>
+    console.log('[SearchFormArea.vue]: script setup completed.');
+    
+    import { ref } from 'vue';
+    
+</script>
+
+<template>
+    <p>SearchResultArea</p>
+    
+</template>
--- data-mng-system/src/components/pages/Search.vue
+++ data-mng-system/src/components/pages/Search.vue
@@ -1,22 +1,35 @@
 <script setup>
     console.log('[Search.vue]: script setup completed.');
     import SearchFormArea from '../layers/SearchFormArea.vue'
+    import SearchResultArea from '../layers/SearchResultArea.vue'
     
 </script>
 
 <template>
   <v-app>
-    <div class="search-area">
+    <div class="search-form-area">
       <SearchFormArea></SearchFormArea>
     </div>
+
+    <div class="search-result-area">
+      <SearchResultArea></SearchResultArea>
+    </div>
+
   </v-app>
 </template>
 
 <style scoped>
-.search-area{
+.search-form-area{
   position: relative;
   padding-left: 0rem;
   background-color: aquamarine;
+  width: 35rem;
+}
+
+.search-result-area{
+  position: relative;
+  padding-left: 0rem;
+  background-color:blanchedalmond;
   width: 35rem;
 }
 </style>
